8cf180aa9ba2bac772bfaf829f84c8d617a37c5a
lewis-huang <huangyun_122@hotmial.com>
2015-03-13 01:50:10 -0500

    attatch the file search tool source code
    attatch the file search tool source code

---
+++ FileSearch/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace FileSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.cmdSearch = new System.Windows.Forms.Button();
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.FullPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dgTargetFiles = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKeyWord = new System.Windows.Forms.TextBox();
+            this.FullPathName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTargetFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(73, 34);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(315, 21);
+            this.txtFolder.TabIndex = 1;
+            this.txtFolder.Text = "txtFolder";
+            // 
+            // cmdSearch
+            // 
+            this.cmdSearch.Location = new System.Drawing.Point(313, 61);
+            this.cmdSearch.Name = "cmdSearch";
+            this.cmdSearch.Size = new System.Drawing.Size(75, 23);
+            this.cmdSearch.TabIndex = 2;
+            this.cmdSearch.Text = "Search";
+            this.cmdSearch.UseVisualStyleBackColor = true;
+            this.cmdSearch.Click += new System.EventHandler(this.cmdSearch_Click);
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.lvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FullPath});
+            this.lvFiles.Location = new System.Drawing.Point(14, 107);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(374, 483);
+            this.lvFiles.TabIndex = 3;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // FullPath
+            // 
+            this.FullPath.Text = "FullPath";
+            this.FullPath.Width = 350;
+            // 
+            // dgTargetFiles
+            // 
+            this.dgTargetFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgTargetFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FullPathName});
+            this.dgTargetFiles.Location = new System.Drawing.Point(405, 13);
+            this.dgTargetFiles.Name = "dgTargetFiles";
+            this.dgTargetFiles.RowTemplate.Height = 23;
+            this.dgTargetFiles.Size = new System.Drawing.Size(1073, 577);
+            this.dgTargetFiles.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "KeyWord";
+            // 
+            // txtKeyWord
+            // 
+            this.txtKeyWord.Location = new System.Drawing.Point(73, 62);
+            this.txtKeyWord.Name = "txtKeyWord";
+            this.txtKeyWord.Size = new System.Drawing.Size(234, 21);
+            this.txtKeyWord.TabIndex = 6;
+            this.txtKeyWord.Text = "txtKeyWord";
+            // 
+            // FullPathName
+            // 
+            this.FullPathName.HeaderText = "FullPathName";
+            this.FullPathName.Name = "FullPathName";
+            this.FullPathName.Width = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1490, 602);
+            this.Controls.Add(this.txtKeyWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgTargetFiles);
+            this.Controls.Add(this.lvFiles);
+            this.Controls.Add(this.cmdSearch);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgTargetFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button cmdSearch;
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.DataGridView dgTargetFiles;
+        private System.Windows.Forms.ColumnHeader FullPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKeyWord;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullPathName;
+    }
+}
+
---
+++ FileSearch/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FileSearch
+{
+    public partial class Form1 : Form
+    {
+        #region global variables set
+        public string sFolder = "";
+        public DirectoryInfo dTarget;
+        public List<FileInfo> fTarget;
+        public string sKeyword = "";
+         FileSystemx targetfs;
+        #endregion global variables set
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdSearch_Click(object sender, EventArgs e)
+        {
+            #region clear control items
+            if ( lvFiles.Items.Count !=0)
+            {
+                lvFiles.Items.Clear();
+            }
+            if (dgTargetFiles.Rows.Count != 0)
+            {
+                dgTargetFiles.Rows.Clear();
+            }
+            #endregion clear control items
+
+            //#region original show up FileSearch result
+            //sFolder = txtFolder.Text;
+            sKeyword = txtKeyWord.Text;
+            //dTarget = new DirectoryInfo(sFolder);
+            //fTarget = new List<FileInfo>();
+            //getRecursiveFiles(dTarget, fTarget);
+            
+            //foreach (FileInfo indFile in fTarget)
+            //{
+            //    lvFiles.Items.Add(indFile.FullName);
+            //    //if (Regex.IsMatch(new StreamReader(indFile.FullName).ReadToEnd(), sKeyword, RegexOptions.IgnoreCase))
+            //    //{
+            //    //    int Rowindex = dgTargetFiles.Rows.Add();
+            //    //    dgTargetFiles.Rows[Rowindex].Cells["FullPathName"].Value = indFile.FullName;
+            //    //}
+            //}
+            //#endregion original show up FileSearch result
+
+            #region backgroup thread used to get the Recursive Files in a folder
+             sFolder = txtFolder.Text;
+            targetfs = new FileSystemx(sFolder);            
+            ThreadPool.QueueUserWorkItem(getRecuriveFilesByBackgroupThread, new object());
+            Thread.Sleep(1000);
+             Int32 endloopindex = targetfs.CurrentFilesCount;
+             Int32 initfileindex = 0;
+                while ( lvFiles.Items.Count  != targetfs.CurrentFilesCount)
+                {
+                    if ( lvFiles.Items.Count != 0 )
+                    {
+                        lvFiles.Clear();
+                    }
+                    while ( initfileindex < endloopindex)
+                    {
+                        lvFiles.Items.Add( targetfs.FolderFilesTarget[initfileindex].FullName);
+                        //lvFiles.Refresh();
+
+                        string searchcontinueflag = "1";
+
+                        #region original method to iterate to apply regular expression on the target file
+                       // StreamReader tmpSr = new StreamReader(targetfs.FolderFilesTarget[initfileindex].FullName);
+                       //StringBuilder tmpStr = new StringBuilder(tmpSr.ReadToEnd());
+                       //try
+                       //{
+                       //    if (Regex.IsMatch(tmpStr.ToString(), sKeyword, RegexOptions.IgnoreCase))
+                       //    {
+                       //        int Rowindex = dgTargetFiles.Rows.Add();
+                       //        dgTargetFiles.Rows[Rowindex].Cells["FullPathName"].Value = targetfs.FolderFilesTarget[initfileindex].FullName;
+                       //        dgTargetFiles.Show();
+                       //    }
+                       //}
+                       //catch (Exception ex)
+                       //{
+                       //    Console.WriteLine("error: " + ex.Message);
+                       //}
+                       //finally
+                       //{
+                       //    tmpStr.Clear();                           
+                       //    tmpSr.Dispose();
+                       //    GC.Collect();
+                       //}
+                        #endregion  original method to iterate to apply regular expression on the target file
+                       
+                       #region method refined to iteratively apply regular expression on the target file
+                        using (StreamReader tmpSr = new StreamReader(targetfs.FolderFilesTarget[initfileindex].FullName))
+                        {
+                            if (new FileInfo(targetfs.FolderFilesTarget[initfileindex].FullName).Length > 100 * 1024 * 1024)
+                            {
+                                #region file size bigger than 100MB
+                                if (!tmpSr.EndOfStream)
+                                {
+                                    if (Regex.IsMatch(tmpSr.ReadLine(), sKeyword, RegexOptions.IgnoreCase) && searchcontinueflag == "1")
+                                    {
+                                        dgTargetFiles.Rows.Add(targetfs.FolderFilesTarget[initfileindex].FullName);
+                                        searchcontinueflag = "0";
+                                    }
+                                }
+                                #endregion file size bigger than 100MB
+                            }
+                            else
+                            {
+                                if (Regex.IsMatch(tmpSr.ReadToEnd(), sKeyword, RegexOptions.IgnoreCase))
+                                {
+                                    dgTargetFiles.Rows.Add(targetfs.FolderFilesTarget[initfileindex].FullName);
+                                    //searchcontinueflag = "0";
+                                }
+                            }
+
+                        }
+                       #endregion method refined to iteratively apply regular expression on the target file
+
+                       initfileindex = initfileindex +1 ;
+                    }
+
+                      Thread.Sleep(1000);
+                    if ( endloopindex != targetfs.CurrentFilesCount)
+                    {
+                        endloopindex = targetfs.CurrentFilesCount;
+                        initfileindex = 0;
+                    }
+                    
+                }
+
+            #endregion backgroup thread used to get the Recursive Files in a folder
+
+
+
+                //foreach (FileInfo indFile in targetfs.FolderFilesTarget)
+                //{
+
+                //    if (Regex.IsMatch(new StreamReader(indFile.FullName).ReadToEnd(), sKeyword, RegexOptions.IgnoreCase))
+                //    {
+                //        int Rowindex = dgTargetFiles.Rows.Add();
+                //        dgTargetFiles.Rows[Rowindex].Cells["FullPathName"].Value = indFile.FullName;
+                //        dgTargetFiles.Show();
+                //    }
+                //}
+
+            
+        }
+
+        private void getRecursiveFiles(DirectoryInfo currentDirectory, List<FileInfo> files)
+        {
+            if (!Regex.IsMatch(currentDirectory.Name.ToString(), "bin", RegexOptions.IgnoreCase))
+            {
+                DirectoryInfo[] iterativeDirectories = currentDirectory.GetDirectories();
+                if (currentDirectory.GetDirectories().Count<DirectoryInfo>() > 0)
+                {
+                    foreach (DirectoryInfo nextDirectory in iterativeDirectories)
+                    {
+                        getRecursiveFiles(nextDirectory, files);
+                    }
+                }
+                foreach (FileInfo currentFile in currentDirectory.GetFiles())
+                {
+                    files.Add(currentFile);
+
+                }
+            }          
+        }
+
+        public void getRecuriveFilesByBackgroupThread(object state)
+        {
+          getRecursiveFilesByBackGroupThread_detail( targetfs, targetfs.DirectoryTarget, targetfs.FolderFilesTarget);
+        }
+         private void getRecursiveFilesByBackGroupThread_detail(FileSystemx targetfs, DirectoryInfo currentDirectory, List<FileInfo> files )
+        {
+            if (!Regex.IsMatch(currentDirectory.Name.ToString(), "bin", RegexOptions.IgnoreCase))
+            {
+                DirectoryInfo[] iterativeDirectories = currentDirectory.GetDirectories();
+                if (currentDirectory.GetDirectories().Count<DirectoryInfo>() > 0)
+                {
+                    foreach (DirectoryInfo nextDirectory in iterativeDirectories)
+                    {
+                        getRecursiveFilesByBackGroupThread_detail(targetfs, nextDirectory, files);
+                    }
+                }
+                foreach (FileInfo currentFile in currentDirectory.GetFiles())
+                {
+                    files.Add(currentFile);                    
+                    targetfs.CurrentFilesCount = targetfs.CurrentFilesCount + 1;
+                }
+            }          
+        }
+
+
+
+    }
+
+    public class FileSystemx
+    {
+        DirectoryInfo Folder;
+        List<FileInfo> FolderFiles;
+        Int32 _CurrentFilesCount = 0;
+        public FileSystemx(string DirectoryPath)
+        {
+            Folder = new DirectoryInfo(DirectoryPath);
+            FolderFiles = new List<FileInfo>();
+        }
+        public Int32 CurrentFilesCount { get { return _CurrentFilesCount; } set { _CurrentFilesCount = value; } }
+        public DirectoryInfo DirectoryTarget { get { return Folder ;} }
+        public List<FileInfo> FolderFilesTarget { get{return FolderFiles;}}
+    }
+}
